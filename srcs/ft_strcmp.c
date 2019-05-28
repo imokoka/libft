acebd937acee2b1113577cedca4d491cd3ea01fd
Itumeleng MOKOKA <imokoka@c4r22s1.wethinkcode.co.za>
2019-05-28 14:27:01 +0200

    Added more shxt

---
+++ srcs/ft_strcmp.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imokoka <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/24 11:27:16 by imokoka           #+#    #+#             */
+/*   Updated: 2019/05/24 13:29:31 by imokoka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_strcmp(const char *s1, const char *s2)
+{
+	int								i;
+	unsigned const char	*str1;
+	unsigned const char	*str2;
+
+	i = 0;
+	str1 = (unsigned const char *)s1;
+	str2 = (unsigned const char *)s2;
+	while (str1[i] == str2[i] && str1[i] != '\0' && str2[i] != '\0')
+		i++;
+	return (str1[i] - str2[i]);
+}
