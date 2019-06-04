f651c27d51fcec0e3973ae82f849dcc1f190cea3
Itumeleng MOKOKA <imokoka@c4r22s1.wethinkcode.co.za>
2019-06-04 14:31:31 +0200

    just for today

---
+++ srcs/ft_strstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imokoka <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/04 13:41:56 by imokoka           #+#    #+#             */
+/*   Updated: 2019/06/04 14:30:58 by imokoka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int i;
+	int j;
+
+	if (ft_strlen(needle) == 0)
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] == haystack[i + j] && haystack[i + j] != '\0'
+		&& needle[j] != '\0')
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)haystack + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
