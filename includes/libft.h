5dcb3049417368f9397535db59855e6f3e12fd87
Itumeleng MOKOKA <imokoka@c4r22s4.wethinkcode.co.za>
2019-06-18 09:16:40 +0200

    Again again

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: imokoka <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:29:41 by imokoka           #+#    #+#             */
-/*   Updated: 2019/06/14 11:55:14 by imokoka          ###   ########.fr       */
+/*   Updated: 2019/06/18 09:11:18 by imokoka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,31 +47,29 @@ void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
 void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
-/*
-**Part 2
-*/
+
 void				ft_putchar(char c);
 void				ft_putchar_fd(char c, int fd);
-void				ft_putstr(char const *s);
-void				ft_putstr_fd(char const *s, int fd);
-void				ft_putendl(char const *s);
-void				ft_putendl_fd(char const *s, int fd);
+void				ft_putstr(const char *s);
+void				ft_putstr_fd(const char *s, int fd);
+void				ft_putendl(const char *s);
+void				ft_putendl_fd(const char *s, int fd);
 void				ft_putnbr(int n);
 void				ft_putnbr_fd(int n, int fd);
 void				ft_strclr(char *s);
 void				*ft_memalloc(size_t size);
 char				*ft_strnew(size_t size);
-int					ft_strequ(char const *s1, char const *s2);
-int					ft_strnequ(char const *s1, char const *s2, size_t n);
+int					ft_strequ(const char *s1, const char *s2);
+int					ft_strnequ(const char *s1, const char *s2, size_t n);
 void				ft_striter(char *s, void (*f)(char *));
 void				ft_striteri(char *s, void (*f)(unsigned int, char *));
-char				*ft_strmap(char const *s, char (*f)(char));
-char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char				*ft_strmap(const char *s, char (*f)(char));
+char				*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 void				ft_strdel(char **as);
 void				ft_memdel(void **ap);
-char				*ft_strjoin(char const *s1, char const *s2);
-char				*ft_strsub(char const *s, unsigned int start, size_t len);
-char				*ft_strtrim(char const *s);
-char				**ft_strsplit(char const *s, char c);
+char				*ft_strjoin(const char *s1, const char *s2);
+char				*ft_strsub(const char *s, unsigned int start, size_t len);
+char				*ft_strtrim(const char *s);
+char				**ft_strsplit(const char *s, char c);
 char				*ft_itoa(int num);
 #endif
