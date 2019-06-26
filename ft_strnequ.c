411a147984fd5e898e1bf3f472e19815c542ef8a
Itumeleng MOKOKA <imokoka@c4r22s4.wethinkcode.co.za>
2019-06-26 13:48:57 +0200

    Done yet again

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: imokoka <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/25 08:37:41 by imokoka           #+#    #+#             */
-/*   Updated: 2019/06/25 08:45:41 by imokoka          ###   ########.fr       */
+/*   Updated: 2019/06/26 13:45:13 by imokoka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (!s1 || !s2)
+	size_t i;
+
+	if (s1 == NULL || s2 == NULL)
 		return (0);
-	if (ft_strncmp(s1, s2, n) == 0)
+	i = 0;
+	if (n == 0)
 		return (1);
-	else
-		return (0);
+	while ((s1[i] != '\0') || (s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return (0);
+		i++;
+		if (i == n)
+			break ;
+	}
+	return (1);
 }
